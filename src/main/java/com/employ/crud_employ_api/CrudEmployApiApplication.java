c70eb47bcab68a5ad2731c5f9beaf4591a6d1d75
Amr Mohamed <amrmuhmedd@gmail.com>
2024-12-18 20:38:18 +0200

    feat: creaet project structure

---
+++ src/main/java/com/employ/crud_employ_api/CrudEmployApiApplication.java
@@ -0,0 +1,15 @@
+package com.employ.crud_employ_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CrudEmployApiApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/employ/crud_employ_api/CrudEmployApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employ.crud_employ_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmployApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
